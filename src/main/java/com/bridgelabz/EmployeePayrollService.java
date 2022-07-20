0014ffec580579d9d72da5756d2ec85f3145abfa
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-07-20 20:58:26 +0530

    Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll Service project.");
+    }
+}
